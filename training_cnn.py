4651bc6b4bfc795096ed505e0f328a18ccfbde6b
Jose <mirzai@fortiss.org>
2018-06-16 14:57:39 +0200

    Preparing the CNN test data.

--- training_cnn.py
+++ training_cnn.py
@@ -6,7 +6,7 @@ Gets to 99.25% test accuracy after 12 epochs
 
 from __future__ import print_function
 import keras
-from keras.datasets import mnist
+#from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
@@ -74,3 +74,4 @@ print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
 model.save_weights("char74k_trained.h5")
+
---
+++ use_cnn.py
@@ -0,0 +1,12 @@
+import keras
+#from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from preparing_dataset import *
+
+
+predictions = model.predict(image)
+
+print()
