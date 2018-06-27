d694504a8ce3cbab7913bf3c71ed1899298988b8
MASTERPC\Administrator <hosein.mirzai@tum.de>
2018-06-27 13:05:57 +0200

    Adding a CMake file for our project and main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 27/06/2018.
+//
+
+#include "main.h"
+#include <iostream>
+
+
+int main(int argc, char* argv[]){
+
+    // From here we launch all of our app
+
+
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 27/06/2018.
+//
+
+#ifndef PROJECT_MAIN_H
+#define PROJECT_MAIN_H
+
+#endif //PROJECT_MAIN_H
